741c2baa13ae9d4cc7a571954ede49886bc24188
Andrey maznyak <andreymaznyak@gmail.com>
2017-12-13 17:16:55 +1000

    add support custom agent (for proxy requests)

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 const http = require('http');
 const https = require('https');
-
+const urlLib = require('url');
 const BEGIN_KEY = '-----BEGIN RSA PUBLIC KEY-----\n';
 const END_KEY = '\n-----END RSA PUBLIC KEY-----\n';
 
-module.exports = function KeycloakPublicKeyFetcher(url, realm) {
-  const certsUrl = realm ? `${url}/auth/realms/${realm}/protocol/openid-connect/certs` : url;
+module.exports = function KeycloakPublicKeyFetcher(url, realm, agent = null) {
+  console.log('req agent:', agent === null);
+  const certsUrl = realm
+    ? `${url}/auth/realms/${realm}/protocol/openid-connect/certs`
+    : url;
   return {
-    fetch: kid => fetch(certsUrl, kid)
+    fetch: kid => fetch(certsUrl, kid, agent)
   };
 };
 
-async function fetch(url, kid) {
-  const response = await getJson(url);
+async function fetch(url, kid, agent) {
+  const response = await getJson(url, agent);
   const key = getKey(response, kid);
   if (!key) {
     throw new Error(`Can't find key for kid "${kid}" in response.`);
@@ -21,30 +24,49 @@ async function fetch(url, kid) {
   return getPublicKey(key.n, key.e);
 }
 
-function getJson(url) {
+function getJson(url, reqAgent) {
   return new Promise((resolve, reject) => {
+    const options = urlLib.parse(url);
+    if (reqAgent !== null) {
+      options.agent = reqAgent;
+    }
     const agent = url.startsWith('https') ? https : http;
-    agent.get(url, (res) => {
-      if (!valid(res)) {
-        res.resume();
-        reject(new Error(`Status: ${res.statusCode}, Content-type: ${res.headers['content-type']}`));
-      }
-      parse(res)
-        .then(result => resolve(result))
-        .catch(error => reject(error));
-    }).on('error', (e) => { reject(e); });
+    agent
+      .get(options, res => {
+        if (!valid(res)) {
+          res.resume();
+          reject(
+            new Error(
+              `Status: ${res.statusCode}, Content-type: ${
+                res.headers['content-type']
+              }`
+            )
+          );
+        }
+        parse(res)
+          .then(result => resolve(result))
+          .catch(error => reject(error));
+      })
+      .on('error', e => {
+        reject(e);
+      });
   });
 }
 
 function valid(response) {
-  return response.statusCode === 200 && response.headers['content-type'] === 'application/json';
+  return (
+    response.statusCode === 200 &&
+    response.headers['content-type'] === 'application/json'
+  );
 }
 
 function parse(response) {
   return new Promise((resolve, reject) => {
     let rawData = '';
     response.setEncoding('utf8');
-    response.on('data', (chunk) => { rawData += chunk; });
+    response.on('data', chunk => {
+      rawData += chunk;
+    });
     response.on('end', () => {
       try {
         const parsedData = JSON.parse(rawData);
@@ -64,7 +86,7 @@ function getKey(response, kid) {
 
 function verify(key) {
   if (!(key.n && key.e)) {
-    throw new Error('Can\'t find modulus or exponent in key.');
+    throw new Error("Can't find modulus or exponent in key.");
   }
   if (key.kty !== 'RSA') {
     throw new Error('Key type (kty) must be RSA.');
@@ -81,34 +103,32 @@ function getPublicKey(modulus, exponent) {
   const exp = convertToHex(exponent);
   const encModLen = encodeLenght(mod.length / 2);
   const encExpLen = encodeLenght(exp.length / 2);
-  const part = [mod, exp, encModLen, encExpLen].map(n => n.length / 2).reduce((a, b) => a + b);
-  const bufferSource = `30${encodeLenght(part + 2)}02${encModLen}${mod}02${encExpLen}${exp}`;
+  const part = [mod, exp, encModLen, encExpLen]
+    .map(n => n.length / 2)
+    .reduce((a, b) => a + b);
+  const bufferSource = `30${encodeLenght(part + 2)}02${encModLen}${mod}02${
+    encExpLen
+  }${exp}`;
   const pubkey = Buffer.from(bufferSource, 'hex').toString('base64');
   return BEGIN_KEY + pubkey.match(/.{1,64}/g).join('\n') + END_KEY;
 }
 
 function convertToHex(str) {
   const hex = Buffer.from(str, 'base64').toString('hex');
-  return hex[0] < '0' || hex[0] > '7'
-    ? `00${hex}`
-    : hex;
+  return hex[0] < '0' || hex[0] > '7' ? `00${hex}` : hex;
 }
 
 function encodeLenght(n) {
-  return n <= 127
-    ? toHex(n)
-    : toLongHex(n);
+  return n <= 127 ? toHex(n) : toLongHex(n);
 }
 
 function toLongHex(number) {
   const str = toHex(number);
-  const lengthByteLength = 128 + (str.length / 2);
+  const lengthByteLength = 128 + str.length / 2;
   return toHex(lengthByteLength) + str;
 }
 
 function toHex(number) {
   const str = number.toString(16);
-  return (str.length % 2)
-    ? `0${str}`
-    : str;
+  return str.length % 2 ? `0${str}` : str;
 }
